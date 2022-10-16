bbb029c2514ec33df57f98a2164c168d25c010b6
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-10-16 18:48:19 +0200

    Ostylowanie statistic

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,15 +1,27 @@
 import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+function getRandomHexColor() {
+  let bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return bgColor;
+}
 
 export const Statistics = ({ title, stats }) => (
-  <div>
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
+  <div className={css.statisticsContainer}>
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
-      <ul className="stat-list">
+      <ul className={css.statList}>
         {stats.map(stat => (
-          <li key={stat.id}>
-            <span className="label">{stat.label}</span>
-            <span className="percentage">{stat.percentage}%</span>
+          <li
+            className={title ? css.statListItem : css.statListItemNoTitle}
+            style={{
+              backgroundColor: getRandomHexColor(),
+            }}
+            key={stat.id}
+          >
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
           </li>
         ))}
       </ul>
