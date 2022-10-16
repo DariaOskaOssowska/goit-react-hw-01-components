e66ffeab66e4283d363346ba79837c811a93e229
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-10-16 20:09:23 +0200

    Ostylowanie friend

--- src/components/friend-list/FriendList.jsx
+++ src/components/friend-list/FriendList.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from './FriendListItem';
+import css from './Friend.module.css'
 
 export const FriendList = ({ friends }) => (
-  <ul className="friend-list">
+  <ul className={css.friendList}>
     {friends.map(friend => (
       <FriendListItem
         key={friend.id}
--- src/components/friend-list/FriendListItem.jsx
+++ src/components/friend-list/FriendListItem.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import css from './Friend.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => (
 
-  <li className="item">
-    <span className="status"></span>
-    <img className="avatar" src={avatar} alt="User avatar" width="48" />
-        <p className="name">{name}</p>
+  <li className={css.item}>
+    <span className={isOnline ? css.online : css.offline}></span>
+    <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+        <p className={css.name}>{name}</p>
   </li>
 );
 
