52fa9d1d5d83de9f5ac602b28c752a05e7a81a4f
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-10-16 21:43:01 +0200

    Ostylowanie transactions

--- src/components/transaction-history/TransactionHistory.jsx
+++ src/components/transaction-history/TransactionHistory.jsx
@@ -1,20 +1,21 @@
 import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
 
 export const TransactionHistory = ({ items }) => (
-  <table className="transaction-history">
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>Amount</th>
-        <th>Currency</th>
+  <table className={css.transactionHistoryTable}>
+    <thead className={css.hederRow}>
+      <tr className={css.valuesRow}>
+        <th className={css.columnHeader}>Type</th>
+        <th className={css.columnHeader}>Amount</th>
+        <th className={css.columnHeader}>Currency</th>
       </tr>
     </thead>
     <tbody>
       {items.map(item => (
-        <tr key={item.id}>
-          <td>{item.type}</td>
-          <td>{item.amount}</td>
-          <td>{item.currency}</td>
+        <tr key={item.id} className={css.valuesRow}>
+          <td className={css.transactionData}>{item.type}</td>
+          <td className={css.transactionData}>{item.amount}</td>
+          <td className={css.transactionData}>{item.currency}</td>
         </tr>
       ))}
     </tbody>
